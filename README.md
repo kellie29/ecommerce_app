1be3d755e9d3d268c75fec256bd9d8eb99a92234
super0605 <36216137+super0605@users.noreply.github.com>
2020-06-23 00:50:58 +0800

    Commit codebase.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ecommerce App
+
+E commerce application using Flutter.
+Users can navigate through different categories and choose the items they like and add them to their favorites or add them to their cart
+
+## Screenshots
+
+<img src="screenshots/categories.jpg" height="720px" >  <img src="screenshots/items.jpg" height="720px" >  <img src="screenshots/itemDetails.jpg" height="720px" >  <img src="screenshots/drawer2.jpg" height="720px" >  
+
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:yala_mazad/home.dart';
+import 'package:yala_mazad/login.dart';
+import 'package:yala_mazad/signUp.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yala Mazad',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: Home(),
+      routes: {
+        '/signUp': (context) => SignUp(),
+        '/login': (context) => Login(),
+        '/home': (context) => Home(),
+        '/feedback': (context) => Home(),
+      },
+    );
+  }
+}
